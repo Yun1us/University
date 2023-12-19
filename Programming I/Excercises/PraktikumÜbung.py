8d7f3cbf276e653a8709a366b34ce114ab3f53a7
s-iadoub <s-iadoub@haw-landshut.de>
2023-12-19 17:00:13 +0100

    Added All Programming Excercises

---
+++ Programming I/Excercises/PraktikumÜbung.py
@@ -0,0 +1,17 @@
+def setKthDigit(n, k, d):
+    """
+    n is an integer, k is a non-negative integer and d is non-negative single digit (0 = d = 9). 
+    Return the number n with the kth digit replaced with d. 
+
+    >>> setKthDigit(468, 0, 1)
+    461
+    >>> setKthDigit(468, 1, 1)
+    418
+    >>> setKthDigit(468, 2, 1)
+    168
+    >>> setKthDigit(468, 3, 1)
+    1468
+    """
+    lst = list(n)
+    print(lst)
+    print ("Hello")
