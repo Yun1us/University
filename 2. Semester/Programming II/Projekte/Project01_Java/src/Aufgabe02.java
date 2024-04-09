7d46a1f1e0d6d361629a3dbebd9a28e5657a9d8f
PC <inoussaadouba@gmail.com>
2024-04-09 16:57:55 +0200

    new changes to java project

--- 2. Semester/Programming II/Projekte/Project01_Java/src/Aufgabe02.java
+++ 2. Semester/Programming II/Projekte/Project01_Java/src/Aufgabe02.java
@@ -1,14 +1,29 @@
 public class Aufgabe02 {
     public static void main(String[] args){
-    switch(args.length){
-        case 1:
-            System.out.println("Kugel: ");
-        case 2:
-            System.out.println("Pyramide: ");
-        case 3:
-            System.out.println("Quader: ");
-        default:
-            System.out.println("Invalid Parameter was given!");
-    }
+        switch(args.length){
+            case 0:
+                System.out.println("Verfügbare Berechnungen: Kugel, Pyramide, Quader");
+                break;
+            case 1:
+                double radius = Double.parseDouble(args[0]);
+                double volumen = (4.0 / 3.0) * Math.PI * Math.pow(radius, 3);
+                System.out.println("Kugelvolumen beträgt: " + volumen);
+                break;
+            case 2:
+                double grundseite = Double.parseDouble(args[0]);
+                double hoehe = Double.parseDouble(args[1]);
+                double volumen2 = (1.0 / 3.0) * Math.pow(grundseite, 2) * hoehe;
+                System.out.println("Pyramidenvolumen beträgt: " + volumen2 );
+                break;
+            case 3:
+                double laenge = Double.parseDouble(args[0]);
+                double breite = Double.parseDouble(args[1]);
+                double hoehe2 = Double.parseDouble(args[2]);
+                double volumen3 = laenge * breite * hoehe2;
+                System.out.println("Quadervolumen beträgt: " + volumen3);
+                break;
+            default:
+                System.out.println("Invalid Parameter was given!");
+        }
     }
 }
