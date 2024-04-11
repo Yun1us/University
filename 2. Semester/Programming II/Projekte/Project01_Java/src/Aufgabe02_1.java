e5f51d5743a0ecba5b77f9a58fb338658ec49d9f
s-iadoub <s-iadoub@haw-landshut.de>
2024-04-11 17:17:55 +0200

    new cahgnes to

---
+++ 2. Semester/Programming II/Projekte/Project01_Java/src/Aufgabe02_1.java
@@ -0,0 +1,13 @@
+public class Aufgabe02_1 {
+    public static void main(String[] args){
+        switch(args.length){
+            case 0:
+                System.out.println("Aufruf: java Flaechenberechner");
+                System.out.println("Verfügbare Berechnungen:");
+                System.out.println("Kugel: Radius");
+                System.out.println("Pyramide: Grundseite Höhe");
+                System.out.println("Quader: Länge Breite Höhe");
+        }
+    }
+
+}
