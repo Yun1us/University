e38926fb7b03a5e3d8aa768f8dc9f4389119b420
s-iadoub <s-iadoub@haw-landshut.de>
2024-04-13 19:34:47 +0200

    added Programmin

--- 2. Semester/Programming II/Projekte/Project01_Java/src/Aufgabe03.java
+++ 2. Semester/Programming II/Projekte/Project01_Java/src/Aufgabe03.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
+import java.util.Random;
 public class Aufgabe03 {
     public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        Random random = new Random();
 
+        System.out.println("Wie viele Aufgaben wollen Sie rechnen?");
+        int numoftask = scan.nextInt();
+        int anzahlrichtig = 0;
+
+        for(int i = 0; i < numoftask; i++){
+            int random1 = random.nextInt(10);
+            int random2 = random.nextInt(10);
+         
+        System.out.printf("Was ist %d * %d\n", random1, random2);
+        int userloesung = scan.nextInt();
+         
+        int correctnum = random1 * random2;
+        if(correctnum == userloesung){
+            anzahlrichtig += 1;
+            System.out.println("Richtig!!!!");
+        }else{
+            System.out.println("Wrong Answer :(");
+        }
+        System.out.printf("Du hast %d von %d Aufgaben!", anzahlrichtig, numoftask);
+
+        
+        }
     }
 }
