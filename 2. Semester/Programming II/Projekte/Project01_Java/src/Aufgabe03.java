a177fea055012f8cbc9203b1768489b2179450b8
s-iadoub <s-iadoub@haw-landshut.de>
2024-04-15 11:30:39 +0200

    closed Scanner

--- 2. Semester/Programming II/Projekte/Project01_Java/src/Aufgabe03.java
+++ 2. Semester/Programming II/Projekte/Project01_Java/src/Aufgabe03.java
@@ -2,11 +2,11 @@ import java.util.Scanner;
 import java.util.Random;
 public class Aufgabe03 {
     public static void main(String[] args){
-        Scanner scan = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         Random random = new Random();
 
         System.out.println("Wie viele Aufgaben wollen Sie rechnen?");
-        int anzahlaufgaben = scan.nextInt();
+        int anzahlaufgaben = scanner.nextInt();
         int anzahlrichtig = 0;
 
         for(int i = 0; i < anzahlaufgaben; i++){
@@ -14,7 +14,7 @@ public class Aufgabe03 {
             int random2 = random.nextInt(10);
 
             System.out.printf("Was ist %d * %d\n", random1, random2);
-            int userloesung = scan.nextInt();
+            int userloesung = scanner.nextInt();
 
             int richtigeloesung = random1 * random2;
             if(richtigeloesung == userloesung){
@@ -27,5 +27,6 @@ public class Aufgabe03 {
         System.out.printf("%d von %d Aufgaben korrekt" , anzahlrichtig, anzahlaufgaben);
         double prozent = (anzahlrichtig * 100)/anzahlaufgaben;
         System.out.println("in Prozent: "+ prozent + "%");
+        scanner.close();
     }
 }
