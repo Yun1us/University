74476015d9f1decf43d765a9264103b3d62cad2a
Yun1us <s-iadoub@haw-landshut.de>
2024-04-18 00:12:17 +0200

    aufgabe4

--- 2. Semester/Programming II/Projekte/Project01_Java/src/Aufgabe04.java
+++ 2. Semester/Programming II/Projekte/Project01_Java/src/Aufgabe04.java
@@ -1,3 +1,24 @@
 public class Aufgabe04 {
-    
+    public static void main(String[] args){
+        int n = 10000;
+        int unterekurvenpunkte = 0;
+        for(int i = 0; i < n; i++){
+        double x = Math.random();
+        double y = Math.random(); 
+        
+        if(y < Funktionswert(x)){
+            unterekurvenpunkte++;               
+        }
+        }
+
+        double schätzung = (double) unterekurvenpunkte / n;
+        System.out.println("Die Schätzung beträgt: " + schätzung);
+
+        
+        
+        
+    }
+    public static double Funktionswert(double x){
+        return x;
+    }
 }
