03265e6a52a96e8c894e305bcb5988b787be6ec0
Yun1us <s-iadoub@haw-landshut.de>
2024-04-21 00:27:25 +0200

    new changes to Aufgabe04

--- 2. Semester/Programming II/Projekte/Project01_Java/src/Aufgabe04.java
+++ 2. Semester/Programming II/Projekte/Project01_Java/src/Aufgabe04.java
@@ -1,4 +1,4 @@
-public class Aufgabe04 {
+/*public class Aufgabe04 {
     public static void main(String[] args){
         int n = 10000;
         int unterekurvenpunkte = 0;
@@ -21,4 +21,27 @@ public class Aufgabe04 {
     public static double Funktionswert(double x){
         return x;
     }
+}*/ 
+public class Aufgabe04{
+    public static void main(String[] args){
+        double MAX_ITERATIONS = 100_000;
+        double MIN_CHANGE = Math.pow(10, -5);
+        double pointsUnderCurve = 0;
+        double approxInt = 0;
+        double allpoints = 0;
+        for(int i = 0; i < MAX_ITERATIONS; i++){
+            double x = Math.random();
+            double y = Math.random();
+            allpoints++;
+
+            if(y <= Funktionswert(x)){
+                pointsUnderCurve++;
+            }
+        }
+        
+    }
+    public static double Funktionswert(double x){
+        return Math.sin(Math.PI) * x;
+
+        }
 }
