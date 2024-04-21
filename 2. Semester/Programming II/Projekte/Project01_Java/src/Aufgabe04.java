b6e93005e9f6cec475e76aedcd4c9a3a465521f0
Yun1us <s-iadoub@haw-landshut.de>
2024-04-21 15:01:31 +0200

    final aufgabe04

--- 2. Semester/Programming II/Projekte/Project01_Java/src/Aufgabe04.java
+++ 2. Semester/Programming II/Projekte/Project01_Java/src/Aufgabe04.java
@@ -22,26 +22,40 @@
         return x;
     }
 }*/ 
-public class Aufgabe04{
-    public static void main(String[] args){
-        double MAX_ITERATIONS = 100_000;
-        double MIN_CHANGE = Math.pow(10, -5);
+public class Aufgabe04 {
+    public static final int MAX_ITERATIONS = 100_000; //Hier sind die 2 Konstanten aufgelistet die in der Aufgabe gelistet sind. 
+    public static final double MIN_CHANGE = 1e-5;
+    
+    public static double Funktionswert(double x) { // wie in der Aufgabenstellung ist hier der Funktionswert wiedergegeben
+        return Math.sin(Math.PI * x);
+    }
+
+    public static void main(String[] args) {
         double pointsUnderCurve = 0;
         double approxInt = 0;
-        double allpoints = 0;
-        for(int i = 0; i < MAX_ITERATIONS; i++){
+        double allPoints = 0;
+        double lastapproxdouble = -1;
+        double diff = 0;
+
+        for (int i = 0; i < MAX_ITERATIONS; i++) {
             double x = Math.random();
             double y = Math.random();
-            allpoints++;
+            allPoints++;
 
             if(y <= Funktionswert(x)){
                 pointsUnderCurve++;
+
+            }
+
+            lastapproxdouble = approxInt;
+            approxInt = pointsUnderCurve / allPoints;
+
+            diff = Math.abs(approxInt - lastapproxdouble);
+
+            if(i > 0 && diff < MIN_CHANGE){ // hier checkt man ab ob i > 0 ist um sicherzustellen dass die schleife mindestens einmal durchgegenagne ist
+                break;
             }
         }
-        
+        System.out.printf("Die Schätzung beträgt: %.5f%n", approxInt);
     }
-    public static double Funktionswert(double x){
-        return Math.sin(Math.PI) * x;
-
-        }
 }
