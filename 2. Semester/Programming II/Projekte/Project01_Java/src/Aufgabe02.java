b5e92c4dd9c8829e4f3006fab2eb626cc4288e90
Yun1us <s-iadoub@haw-landshut.de>
2024-04-22 17:21:18 +0200

    changes literally nothing

--- 2. Semester/Programming II/Projekte/Project01_Java/src/Aufgabe02.java
+++ 2. Semester/Programming II/Projekte/Project01_Java/src/Aufgabe02.java
@@ -1,25 +1,25 @@
 public class Aufgabe02 {
     public static void main(String[] args){
-        switch(args.length){
-            case 0:
+        switch(args.length){ // switch case mit args lenght als expression 
+            case 0: // case 0 wenn keine Argumente eingegeben werden 
                 System.out.println("Verfügbare Berechnungen: Kugel, Pyramide, Quader");
                 break;
-            case 1:
-                double radius = Double.parseDouble(args[0]);
-                double volumen = (4.0 / 3.0) * Math.PI * Math.pow(radius, 3);
-                System.out.println("Kugelvolumen beträgt: " + volumen);
+            case 1: // case 1 wenn 1 Argument als Parameter eingegeben wird
+                double radius = Double.parseDouble(args[0]); //Array an der Stelle 0 wird in eine double
+                double volumen = (4.0 / 3.0) * Math.PI * Math.pow(radius, 3); // Volumenberechnung 
+                System.out.println("Kugelvolumen beträgt: " + volumen); 
                 break;
-            case 2:
-                double grundseite = Double.parseDouble(args[0]);
+            case 2: // case 2 bei 2 Arguments als Parameter 
+                double grundseite = Double.parseDouble(args[0]); 
                 double hoehe = Double.parseDouble(args[1]);
-                double volumen2 = (1.0 / 3.0) * Math.pow(grundseite, 2) * hoehe;
-                System.out.println("Pyramidenvolumen beträgt: " + volumen2 );
+                double volumen2 = (1.0 / 3.0) * Math.pow(grundseite, 2) * hoehe; //Volumenbrecehnung einer Pyramide
+                System.out.println("Pyramidenvolumen beträgt: " + volumen2 ); 
                 break;
             case 3:
                 double laenge = Double.parseDouble(args[0]);
                 double breite = Double.parseDouble(args[1]);
                 double hoehe2 = Double.parseDouble(args[2]);
-                double volumen3 = laenge * breite * hoehe2;
+                double volumen3 = laenge * breite * hoehe2; //Volumenberechnung eines Quaders 
                 System.out.println("Quadervolumen beträgt: " + volumen3);
                 break;
             default:
