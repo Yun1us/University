a35add87577267b8683423e8eca8a78b99e54c9d
Yun1us <s-iadoub@haw-landshut.de>
2024-04-22 17:22:55 +0200

    added m³

--- 2. Semester/Programming II/Projekte/Project01_Java/src/Aufgabe02.java
+++ 2. Semester/Programming II/Projekte/Project01_Java/src/Aufgabe02.java
@@ -7,20 +7,20 @@ public class Aufgabe02 {
             case 1: // case 1 wenn 1 Argument als Parameter eingegeben wird
                 double radius = Double.parseDouble(args[0]); //Array an der Stelle 0 wird in eine double
                 double volumen = (4.0 / 3.0) * Math.PI * Math.pow(radius, 3); // Volumenberechnung 
-                System.out.println("Kugelvolumen beträgt: " + volumen); 
+                System.out.println("Kugelvolumen beträgt: " + volumen + "m³"); 
                 break;
             case 2: // case 2 bei 2 Arguments als Parameter 
                 double grundseite = Double.parseDouble(args[0]); 
                 double hoehe = Double.parseDouble(args[1]);
                 double volumen2 = (1.0 / 3.0) * Math.pow(grundseite, 2) * hoehe; //Volumenbrecehnung einer Pyramide
-                System.out.println("Pyramidenvolumen beträgt: " + volumen2 ); 
+                System.out.println("Pyramidenvolumen beträgt: " + volumen2 + "m³" ); 
                 break;
             case 3:
                 double laenge = Double.parseDouble(args[0]);
                 double breite = Double.parseDouble(args[1]);
                 double hoehe2 = Double.parseDouble(args[2]);
                 double volumen3 = laenge * breite * hoehe2; //Volumenberechnung eines Quaders 
-                System.out.println("Quadervolumen beträgt: " + volumen3);
+                System.out.println("Quadervolumen beträgt: " + volumen3 + "m³");
                 break;
             default:
                 System.out.println("Invalid Parameter was given!");
