84b01061018a9678e266ec46879f01724b00ceb1
Yun1us <s-iadoub@haw-landshut.de>
2024-04-22 20:19:59 +0200

    new commit

--- 2. Semester/Programming II/Projekte/Project01_Java/src/Aufgabe04.java
+++ 2. Semester/Programming II/Projekte/Project01_Java/src/Aufgabe04.java
@@ -13,25 +13,25 @@ public class Aufgabe04 {
         double lastapproxdouble = -1;
         double diff = 0;
 
-        for (int i = 0; i < MAX_ITERATIONS; i++) {
+        for (int i = 0; i < MAX_ITERATIONS; i++) { // Schleife in der  2 randoms erstellt werden und all points um 1 pro durchlauf erhöht wird 
             double x = Math.random();
             double y = Math.random();
             allPoints++;
 
-            if(y <= Funktionswert(x)){
+            if(y <= Funktionswert(x)){ // Wenn der random y wert and der y stelle des funktionswerts kleiner ist dann wird pointsundercurve um 1 erhöht 
                 pointsUnderCurve++;
 
             }
 
-            lastapproxdouble = approxInt;
-            approxInt = pointsUnderCurve / allPoints;
+            lastapproxdouble = approxInt; // Speichert den letzten integer von approxInt 
+            approxInt = pointsUnderCurve / allPoints; // Hier wird die Schätzung berechnet 
 
-            diff = Math.abs(approxInt - lastapproxdouble);
+            diff = Math.abs(approxInt - lastapproxdouble); // eine differenz wird gebildet um später die änderung zu prüfen  
 
             if(i > 0 && diff < MIN_CHANGE){ // hier checkt man ab ob i > 0 ist um sicherzustellen dass die schleife mindestens einmal durchgegenagne ist
-                break;
+                break; // wenn nicht, dann wird hier die Schleife unterbrochen 
             }
         }
-        System.out.printf("Die Schätzung beträgt: %.5f%n (%f)", approxInt, allPoints);
+        System.out.printf("Die Schätzung beträgt: %.5f%n (%f)", approxInt, allPoints); //formatierung von argumenten die dann ausgegeben werden 
     }
 }
