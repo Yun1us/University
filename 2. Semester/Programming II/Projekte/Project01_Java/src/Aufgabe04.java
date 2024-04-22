234467d55e52ee84ad57cb3a27301e9ced55f8a1
Yun1us <s-iadoub@haw-landshut.de>
2024-04-22 20:27:07 +0200

    aufgabe04 changes

--- 2. Semester/Programming II/Projekte/Project01_Java/src/Aufgabe04.java
+++ 2. Semester/Programming II/Projekte/Project01_Java/src/Aufgabe04.java
@@ -2,15 +2,15 @@ public class Aufgabe04 {
     public static final int MAX_ITERATIONS = 100_000; //Hier sind die 2 Konstanten aufgelistet die in der Aufgabe gelistet sind. 
     public static final double MIN_CHANGE = 1e-5;
     
-    public static double Funktionswert(double x) { // wie in der Aufgabenstellung ist hier der Funktionswert wiedergegeben
-        return Math.sin(Math.PI * x);
+    public static double Funktionswert(double x) { //Funktionswert methode 
+        return Math.sin(Math.PI * x); // berechnet den wert der sinusfunktion für das gegebene x  und skaliert den einheitsintervall [0,1] auf [0,PI]
     }
 
     public static void main(String[] args) {
         double pointsUnderCurve = 0;
         double approxInt = 0;
         double allPoints = 0;
-        double lastapproxdouble = -1;
+        double lastapproxdouble = -1; // wird auf -1 gesetzt um bei der ersten berechnung von diff  einen gültigen vergleichswert zu haben
         double diff = 0;
 
         for (int i = 0; i < MAX_ITERATIONS; i++) { // Schleife in der  2 randoms erstellt werden und all points um 1 pro durchlauf erhöht wird 
