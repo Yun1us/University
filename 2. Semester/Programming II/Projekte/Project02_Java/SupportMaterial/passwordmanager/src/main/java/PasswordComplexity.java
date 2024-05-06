03e72a39fd406534913d78f6ed89a0c9f3d6b408
Yun1us <s-iadoub@haw-landshut.de>
2024-05-06 16:19:26 +0200

    new file added

--- 2. Semester/Programming II/Projekte/Project02_Java/SupportMaterial/passwordmanager/src/main/java/PasswordComplexity.java
+++ 2. Semester/Programming II/Projekte/Project02_Java/SupportMaterial/passwordmanager/src/main/java/PasswordComplexity.java
@@ -1,34 +1,34 @@
 import java.util.Random;
-public enum PasswordComplexity {
-    PIN("0", 4),
+public enum PasswordComplexity {    //enum PasswordComplexity
+    PIN("0", 4),    // festlegung der Konstanten und deren chars sowie auch lengths
     SIMPLE("aA", 5),
     MEDIUM("aA0", 8),
     COMPLEX("aA0!", 10),
     SUPER_COMPLEX("aA0!", 16);
 
-    private final String chars;
+    private final String chars; //initialisierung von chars und length
     private final int length;
 
-PasswordComplexity(String chars, int length){
+PasswordComplexity(String chars, int length){ //Konstruktor der PasswordComplexity enum
     this.chars = chars;
     this.length = length;
 }
 
-public String getChars(){
+public String getChars(){ //getter methode chars die chars returned
     return chars;
 }
-public int getLength(){
+public int getLength(){ //getter methode geLength die die länge returned
     return length;
 }
 
-public String generatePassword(){
-    Random random = new Random();
-    String allowedChars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!?+-;,.:";
-    StringBuilder password = new StringBuilder();
-    for(int i = 0; i < length; i++){
-        switch(this){
+public String generatePassword(){ //generate password generiert passwörter 
+    Random random = new Random(); //initialisierung von Random
+    String allowedChars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!?+-;,.:"; //allowedChars variable die alle erlaubten charakter beinhaltet
+    StringBuilder password = new StringBuilder(); //effektivere weise an den String password dinge zu appenden 
+    for(int i = 0; i < length; i++){ //schleife 
+        switch(this){ //switcher der zwischen die Complexities switcht  
             case PIN:
-            password.append(allowedChars.charAt(random.nextInt(10)));
+            password.append(allowedChars.charAt(random.nextInt(10))); //gibt eine random zahl von 0-9 wieder die auf den index von allowedChars basiert.
             break;
             case SIMPLE:
             password.append(allowedChars.charAt(random.nextInt(10,allowedChars.length()-8)));
@@ -41,7 +41,7 @@ public String generatePassword(){
             break;
         }
     }
-    return password.toString();
+    return password.toString(); //password wird in ein immutable String verwandelt
     }
 }
 
--- 2. Semester/Programming II/Projekte/Project02_Java/SupportMaterial/passwordmanager/src/main/java/PasswordStore.java
+++ 2. Semester/Programming II/Projekte/Project02_Java/SupportMaterial/passwordmanager/src/main/java/PasswordStore.java
@@ -23,31 +23,45 @@ public class PasswordStore {
     }
 
     public boolean contains(PasswordEntry entry){
-        PasswordEntry current = firstEntry;
         if (entry == null){
             throw new IllegalArgumentException("entry darf nicht null sein!");
         }
-        while(current != null){
-            if (current.equals(entry)){
-                return true;
-            }
-            current = current.getNextEntry();
+        if (this.firstEntry == null) {
+            return false;
         }
-        return false;        
+
+        PasswordEntry current = firstEntry;
+        //while(current != null){
+        //if (current.equals(entry)){
+        //        return true;
+        //    }
+        //    current = current.getNextEntry();
+        //}
+        while (true) {
+            if (current.equals(entry)) return true;
+            if (current.getNextEntry() == null) return false;
+            current = current.getNextEntry(); 
+        }
     }
     public boolean add(PasswordEntry entry){
         if(entry == null){
             throw new IllegalArgumentException("entry darf nicht null sein!");
         }
-        if(contains(entry)){
+        if(this.contains(entry)){
             return false;
         }
-        else{
-            entry.setNextEntry(firstEntry);
-            setFirstEntry(entry); 
-            size++;
+
+        if(this.firstEntry == null){
+            this.firstEntry = entry;
+            this.size++;
             return true;
         }
+
+        entry.setNextEntry(firstEntry);
+        this.setFirstEntry(entry); 
+        this.size++;
+        return true;
+        
     }
 }
 
