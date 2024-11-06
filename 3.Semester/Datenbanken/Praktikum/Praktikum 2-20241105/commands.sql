428e3d13323aaa6d1163fcec317d9bd80e50aea9
Yun1us <s-iadoub@haw-landshut.de>
2024-11-06 20:17:42 +0100

    SQL

--- 3.Semester/Datenbanken/Praktikum/Praktikum 2-20241105/commands.sql
+++ 3.Semester/Datenbanken/Praktikum/Praktikum 2-20241105/commands.sql
@@ -5,3 +5,5 @@ select * from bank;
 select geschaeftssitz from bank: 
 select mname, wohnort from Mitarbeiter where anstellungsdatum > "1998-04-28" order by mname;
 select mname, wohnort, bname, blz from Mitarbeiter, bank where arbeitgeber_bank = bname and monatsgehalt < 2500;
+select mname, wohnort, bname, blz from Mitarbeiter join bank on arbeitgeber_bank = bname where monatsgehalt < 2500;
+select kontoart, kontonummer from konto, unterhaelt, kunde where kontonummer = kontonr and inhaber = kname and betreuer = "Olafson";
